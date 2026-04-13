6afc3e5ba6d2b64276305a88ee9f13d572bb3a80
Yves-Tabor <walvesoutis.gmail.com>
2026-04-13 09:22:56 +0200

    Initial commit for my Calculator app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <walves>
+      <main className="container">
+
+      </main>
+    </walves>
+  )
+}
+
+export default App
